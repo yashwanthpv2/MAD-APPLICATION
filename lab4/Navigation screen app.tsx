18b8c805f6deaf2189dfc8f3cd5beea098a08d3d
yashwanthpv2 <yashwanthpvbca24@rvu.edu.in>
2026-04-16 21:37:30 +0530

    Create Navigation screen app.tsx

---
+++ lab4/Navigation screen app.tsx
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./HomeScreen";
+import StudentScreen from "./StudentScreen";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Student" component={StudentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
